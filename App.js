152c5436d524fccd4381e1ed6522948f065180e3
DrCRajeshkannan <rajeshkannanme@gmail.com>
2024-10-02 12:33:29 +0530

    App file added

---
+++ App.js
@@ -0,0 +1,16 @@
+// File name: App.js
+const calculator = require('./calc');
+ 
+let x = 50, y = 20;
+ 
+console.log("Addition of 50 and 20 is "
+    + calculator.add(x, y));
+ 
+console.log("Subtraction of 50 and 20 is "
+    + calculator.sub(x, y));
+ 
+console.log("Multiplication of 50 and 20 is "
+    + calculator.mult(x, y));
+ 
+console.log("Division of 50 and 20 is "
+    + calculator.div(x, y));
